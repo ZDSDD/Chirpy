21874ab8a021e74c602b07f7eef532c1100f2f42
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2024-01-22 18:31:02 +0100

    add function to clean the chirp from bad words

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -24,6 +25,7 @@ func main() {
 	apiRouter := chi.NewRouter()
 	apiRouter.Get("/healthz", readinessHandler)
 	apiRouter.Get("/reset", apiCfg.resetHandler)
+	apiRouter.Post("/validate_chirp", validateChirpHandler)
 	router.Mount("/api", apiRouter)
 
 	adminRouter := chi.NewRouter()
@@ -45,20 +47,78 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8 ")
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
+
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte(fmt.Sprint("<h1>Welcome, Chirpy Admin</h1>")))
 	w.Write([]byte(fmt.Sprintf("<p>Chirpy has been visited %d times!</p>", cfg.fileserverHits)))
 	w.WriteHeader(http.StatusOK)
 }
+
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits = 0
 	w.WriteHeader(http.StatusOK)
 }
+
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits += 1
 		//w.Header().Set("Cache-Control", "no-cache")
 		next.ServeHTTP(w, r)
 	})
+}
+
+type Chirp struct {
+	Body string `json:"body"`
+}
+
+type ChirpError struct {
+	Error string `json:"error"`
+}
+
+type ChirpValid struct {
+	Valid bool `json:"valid"`
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+
+	decoder := json.NewDecoder(r.Body)
+	chirp := Chirp{}
+	err := decoder.Decode(&chirp)
+
+	if err != nil {
+		// an error will be thrown if the JSON is invalid or has the wrong types
+		// any missing fields will simply have their values in the struct set to their zero value
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	if len(chirp.Body) >= 140 {
+		w.WriteHeader(400)
+		chirpError, err := json.Marshal(ChirpError{
+			Error: "Chirp is too long",
+		})
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Write(chirpError)
+		return
+	}
+
+	// params is a struct with data populated successfully
+
+
+
+	validChirp, err := json.Marshal(Chirp{
+		Body: cleanBody(chirp.Body),
+	})
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(validChirp)
 }
